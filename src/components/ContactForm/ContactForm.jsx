8abdb0090210024cfe9bd6c59c7c54bac89e1c8f
Vitalii <65967741+SolonukhaVitalii@users.noreply.github.com>
2021-01-18 09:13:23 +0200

    DZ2 - work 2 redag

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+//import PropTypes from 'prop-types';
+
+const ContactForm = props => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+ContactForm.propTypes = {
+    
+};
+
+export default ContactForm;
